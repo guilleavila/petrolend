b4d8e5c3ba7db2210d726facc812156892086cfd
Guille Ávila <guille9797@hotmail.com>
2022-02-15 11:31:47 +0100

    asd

--- petrol-code/googleroutes.js
+++ petrol-code/googleroutes.js
@@ -3,12 +3,12 @@
 
 https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.4169694%2C-3.7034544&radius=1500&type=gas_station&key=AIzaSyCNqmPenrVlA0TSc1T5hMFVJxUmp7YPYjg
 
-    
+
 
 // Ruta para sacar la información de cada una de las gasolineras, pasándole su ID --> PLACE DETAILS
 
 https://maps.googleapis.com/maps/api/place/details/json
-  ? place_id = ChIJi6C1MxquEmsR9 - c - 3O48ykI
+  //? place_id = ChIJi6C1MxquEmsR9 - c - 3O48ykI
     & key=AIzaSyCNqmPenrVlA0TSc1T5hMFVJxUmp7YPYjg
 
 
