6c6e1ff842cca5871d2ad630e2224536103e0461
riiGalaxy <riimolpeceres@gmail.com>
2022-02-15 17:05:09 +0100

    done

--- petrol-code/app.js
+++ petrol-code/app.js
@@ -9,21 +9,18 @@ const hbs = require("hbs");
 const app = express();
 
 require("./config")(app);
-require('./config/session.config')(app)
+require("./config/session.config")(app);
 
 app.locals.appTitle = `PETROL-END`;
 
 const indexRouter = require("./routes/index.routes");
 app.use("/", indexRouter);
 
-<<<<<<< HEAD
+const authRouter = require("./routes/auth.routes");
+app.use("/", authRouter);
+
 const vehicleRouter = require("./routes/vehicle/vehicle.routes");
 app.use("/vehiculos", vehicleRouter);
-=======
-const authRouter = require('./routes/auth.routes')
-app.use('/', authRouter)
-
->>>>>>> c7abb6d3d2537d34bc033beedc6d0dbdf89dbd5f
 
 require("./error-handling")(app);
 
